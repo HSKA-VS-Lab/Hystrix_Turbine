cc221427bd25fc8770c38e136953b89dfb4c7289
Marls2910 <marleen.eisenacht@gmx.de>
2020-01-09 14:05:32 +0100

    hystrix dashboard configs

--- src/main/java/de/hska/iwi/vslab/Hystrix_Turbine/HystrixTurbineApplication.java
+++ src/main/java/de/hska/iwi/vslab/Hystrix_Turbine/HystrixTurbineApplication.java
@@ -2,8 +2,14 @@ package de.hska.iwi.vslab.Hystrix_Turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
+@EnableDiscoveryClient
+@EnableHystrixDashboard
 public class HystrixTurbineApplication {
 
 	public static void main(String[] args) {
