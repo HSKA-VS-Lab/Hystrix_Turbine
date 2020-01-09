441e4645a634fe78cc9a62b156e998f7c05ae54a
Marls2910 <marleen.eisenacht@gmx.de>
2020-01-09 13:57:11 +0100

    initial commit

---
+++ src/main/java/de/hska/iwi/vslab/Hystrix_Turbine/HystrixTurbineApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Hystrix_Turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixTurbineApplication.class, args);
+	}
+
+}
